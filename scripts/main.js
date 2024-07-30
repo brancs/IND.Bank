7107c3e46889c93554e71729b3c06aa0edc8b4ba
Vitor Brancalião <vitor.bsilva@iob.com.br>
2024-07-30 12:25:53 -0300

    Add gsap

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger);
+  const myText = new SplitType(".txt-to-animate", { types: 'words' });
+  const txtToAnimateList = gsap.utils.toArray(".txt-to-animate");
+
+  txtToAnimateList.forEach(txtToAnimate => {
+    const txtToAnimateSelector = gsap.utils.selector(txtToAnimate);
+    gsap.to(txtToAnimateSelector(".word"), {
+      y: 0,
+      stagger: 0.15,
+      delay: 0.1,
+      duration: 0.2,
+      scrollTrigger: {
+        trigger: txtToAnimate,
+        markers: true
+      }
+    });
+  });
+
+
+ });
