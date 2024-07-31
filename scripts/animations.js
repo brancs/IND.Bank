1e35099025ec9a7cdead57565479cbcc806af58e
Vitor Brancalião <vitor.bsilva@iob.com.br>
2024-07-31 17:21:14 -0300

    Organize files and change card orientation

---
+++ scripts/animations.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger);
+  const myText = new SplitType(".txt--animation", { types: 'words' });
+  const txtToAnimateList = gsap.utils.toArray(".txt--animation");
+
+  txtToAnimateList.forEach(txtToAnimate => {
+    const txtToAnimateSelector = gsap.utils.selector(txtToAnimate);
+    gsap.to(txtToAnimateSelector(".word"), {
+      y: 0,
+      stagger: 0.15,
+      delay: 0.1,
+      duration: 0.2,
+      scrollTrigger: {
+        trigger: txtToAnimate,
+      }
+    });
+  });
+
+  // gsap.to((".card--animation .card"), {
+  //   y: 0,
+  //   scale: 1,
+  //   opacity: 1,
+  //   delay: 0.1,
+  //   duration: 0.1,
+  //   scrollTrigger: {
+  //     trigger: ".card--animation",
+  //   }
+  // });
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,10 @@
+const today = new Date();
+
 document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollTrigger);
-  const myText = new SplitType(".txt--animation", { types: 'words' });
-  const txtToAnimateList = gsap.utils.toArray(".txt--animation");
+  setFooterYear();
+});
 
-  txtToAnimateList.forEach(txtToAnimate => {
-    const txtToAnimateSelector = gsap.utils.selector(txtToAnimate);
-    gsap.to(txtToAnimateSelector(".word"), {
-      y: 0,
-      stagger: 0.15,
-      delay: 0.1,
-      duration: 0.2,
-      scrollTrigger: {
-        trigger: txtToAnimate,
-      }
-    });
-  });
-
-  gsap.to((".card--animation .card"), {
-    y: 0,
-    opacity: 1,
-    delay: 0.1,
-    duration: 0.2,
-    scrollTrigger: {
-      trigger: ".card--animation",
-      markers: true
-    }
-  });
- });
+ function setFooterYear() {
+  const footerYear = document.querySelector(".footer-year__txt");
+  footerYear.textContent = today.getFullYear();
+ }
