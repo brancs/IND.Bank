0603e313c9231d15121a88510e94218bc2532989
Vitor Brancalião <vitor.bsilva@iob.com.br>
2024-07-31 16:21:05 -0300

    Card animation on gsap

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
-  const myText = new SplitType(".txt-to-animate", { types: 'words' });
-  const txtToAnimateList = gsap.utils.toArray(".txt-to-animate");
+  const myText = new SplitType(".txt--animation", { types: 'words' });
+  const txtToAnimateList = gsap.utils.toArray(".txt--animation");
 
   txtToAnimateList.forEach(txtToAnimate => {
     const txtToAnimateSelector = gsap.utils.selector(txtToAnimate);
@@ -16,5 +16,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
   });
 
-
+  gsap.to((".card--animation .card"), {
+    y: 0,
+    opacity: 1,
+    delay: 0.1,
+    duration: 0.2,
+    scrollTrigger: {
+      trigger: ".card--animation",
+      markers: true
+    }
+  });
  });
